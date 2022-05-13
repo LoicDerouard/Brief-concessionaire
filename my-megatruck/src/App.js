3d55e72c74f52c7b76e084829997254dd314974e
GitPatout <patout.julien.02@gmail.com>
2022-05-13 10:35:17 +0200

    changement

--- my-megatruck/src/App.js
+++ my-megatruck/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState, useEffect } from 'react';
+// import "./main.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+// export default function App() {
+//   const [data, setData] = useState(null);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+//   const [query, setQuery] = useState("")
+
+
+
+//   useEffect(() => {
+//     const getData = async () => {
+//       try {
+//         const response = await fetch('https://dummyjson.com/products');
+//         if (!response.ok) {
+//           throw new Error(
+//             `This is an HTTP error: The status is ${response.status}`
+//           );
+//         }
+//         let actualData = await response.json();
+//         setData(actualData.products);
+//         setError(null);
+//       } catch (err) {
+//         setError(err.message);
+//         setData(null);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+//     getData()
+//   }, []);
+
+//   return <div className='App'>
+//     <h1>API Posts</h1>
+//     {loading && <div>A moment please...</div>}
+//     {error && (
+//       <div>{`There is a problem fetching the post data - ${error}`}</div>
+//       )}
+//       <input placeholder='Enter Post Title' onChange={event => setQuery(event.target.value)} />
+//     <ul>
+//       {data &&
+//         data.filter(post => {
+//           if (query === '') {
+//             return post;
+//           } else if (post.title.toLowerCase().includes(query.toLowerCase())) {
+//             return post;
+//           }
+//         }).map(({ id, title, description, thumbnail, price }) => (
+//           <li key={id}>
+//             <img src={thumbnail} />
+//             <h3>{title}</h3>
+//             <p>{description}</p>
+              
+
+//             <p>{price}</p>
+//           </li>
+//         ))}
+//     </ul>
+
+
+
+
+    
+//   </div>
+// }
