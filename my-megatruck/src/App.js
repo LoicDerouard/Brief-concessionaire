b7607e1604927d9cae91461df837446d737af6ae
GitPatout <patout.julien.02@gmail.com>
2022-05-13 15:56:13 +0200

    modif page index et scss supprimer

--- my-megatruck/src/App.js
+++ my-megatruck/src/App.js
@@ -1,67 +1,53 @@
-// import { useState, useEffect } from 'react';
-// import "./main.css";
-
-
-// export default function App() {
-//   const [data, setData] = useState(null);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-//   const [query, setQuery] = useState("")
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
 
 
-//   useEffect(() => {
-//     const getData = async () => {
-//       try {
-//         const response = await fetch('https://dummyjson.com/products');
-//         if (!response.ok) {
-//           throw new Error(
-//             `This is an HTTP error: The status is ${response.status}`
-//           );
-//         }
-//         let actualData = await response.json();
-//         setData(actualData.products);
-//         setError(null);
-//       } catch (err) {
-//         setError(err.message);
-//         setData(null);
-//       } finally {
-//         setLoading(false);
-//       }
-//     }
-//     getData()
-//   }, []);
+export default function App() {
+  const [query, setQuery] = useState("")
+  const [data, setData] = useState(null);
 
-//   return <div className='App'>
-//     <h1>API Posts</h1>
-//     {loading && <div>A moment please...</div>}
-//     {error && (
-//       <div>{`There is a problem fetching the post data - ${error}`}</div>
-//       )}
-//       <input placeholder='Enter Post Title' onChange={event => setQuery(event.target.value)} />
-//     <ul>
-//       {data &&
-//         data.filter(post => {
-//           if (query === '') {
-//             return post;
-//           } else if (post.title.toLowerCase().includes(query.toLowerCase())) {
-//             return post;
-//           }
-//         }).map(({ id, title, description, thumbnail, price }) => (
-//           <li key={id}>
-//             <img src={thumbnail} />
-//             <h3>{title}</h3>
-//             <p>{description}</p>
-              
 
-//             <p>{price}</p>
-//           </li>
-//         ))}
-//     </ul>
+  useEffect( () => {
+    getAllData() ;
+
+
+},[]) ;
+
+  const getAllData = () => {
+
+    axios.get('bd/megatruck.json')
+    .then(res => {
+      const p = res.data;
+      setData( p );
+
+    });
+
+  }
+  return <div className='App'>
+    <h1>Nos Camions</h1>
+      <input placeholder='Recherche' onChange={event => setQuery(event.target.value)} />
+    <ul>
+      {data &&
+        data.filter(post => {
+          if (query === '') {
+            return post;
+          } else if (post.title.toLowerCase().includes(query.toLowerCase())) {
+            return post;
+          }
+        }).map(({ id, title, price, images }) => (
+          <li key={id}>
+            <img src={images} />
+            <h3 className='titre'>{title}</h3>
+            <p className='prix'>{price}</p>
+          </li>
+        ))}
+    </ul>
 
 
 
 
     
-//   </div>
-// }
+  </div>
+}
